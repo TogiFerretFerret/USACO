cb12a884f3b1c7ea888e87810f12ee7a9593a632
Riverxia <riverdalecoder@outlook.com>
2026-01-09 20:13:34 -0500

    bucbuc

--- ext.py
+++
@@ -1,5 +0,0 @@
-f=dict(zip([f"p{x}.txt" for x in list("abc")],[f"p{x}.html" for x in list("abc")]));
-for v in list(f.keys()):
-    with open(f[v],"r") as z:
-        with open(v,"w") as g: 
-            g.write(__import__("bs4").BeautifulSoup(z.read(),features="html.parser").get_text())
