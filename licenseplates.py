2d573097ada063b11d217dcf463ccfcb3fa8feea
Riverxia <riverdalecoder@outlook.com>
2026-01-12 13:49:13 -0500

    favicon

---
+++ licenseplates.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'verifyPlate' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING plate
+#  2. STRING day
+#
+
+def verifyPlate(plate, day):
+    # Write your code here
+    return ""
+if __name__ == '__main__':
+    plate = input()
+    day = input()
+    result = verifyPlate(plate, day)
+    print(result)
+
