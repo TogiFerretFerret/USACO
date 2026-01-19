b8200cedce3fb1b4cfb7a6d54642b26b3716e4e0
Riverxia <riverdalecoder@outlook.com>
2026-01-19 16:54:26 -0500

    story about buying my classmate once a week reference

---
+++ solved/H. Shiori Miyagi and Maximum Array Score.cpp
@@ -0,0 +1,84 @@
+// Problem: H. Shiori Miyagi and Maximum Array Score
+// Judge: Codeforces
+// URL: https://codeforces.com/contest/2171/problem/H
+// Memory Limit: 256 MB
+// Time Limit: 4000 ms
+//
+// story about buying my classmate once a week? not sure
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+static const uint64_t CHRONO_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+static const uint64_t PIDRNDP = (uint64_t)(getpid())*0xbffbffbffULL;
+static mt19937_64 PIDRNG(PIDRNDP);
+static const uint64_t PIDRND = PIDRNG();
+static const uint64_t FIXED_RANDOM = CHRONO_RANDOM ^ PIDRND;
+struct CHASH {
+    template <typename T> size_t operator()(const T& x) const {
+        return hash<T>{}(x) ^ FIXED_RANDOM;
+    }
+    template <typename T1, typename T2> size_t operator()(const pair<T1, T2>& x) const {
+        return (*this)(x.first) ^ ((*this)(x.second) + 0x9e3779b9 + (x.first << 6) + (x.first >> 2));
+    }
+};
+template<class T, class U> istream& operator>>(istream& i, pair<T, U>& p) { return i >> p.first >> p.second; }
+template<class T, class U> ostream& operator<<(ostream& o, const pair<T, U>& p) { return o << p.first << " " << p.second; }
+template<class T> istream& operator>>(istream& i, vector<T>& v) { for(auto& x : v) i >> x; return i; }
+template<class T> ostream& operator<<(ostream& o, const vector<T>& v) { for(int i=0; i<v.size(); ++i) o << v[i] << (i==v.size()-1?"":" "); return o; }
+#define m1(x) template<class T, class... U> void x(T&& a, U&&... b)
+#define m2(x) (int[]){(x forward<U>(b),0)...}
+m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << "\n"; }
+m1(debug) { cerr << forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
+m1(in) { cin >> forward<T>(a); m2(cin >>); }
+template<typename T1,typename T2> using hashmap=unordered_map<T1,T2,CHASH>;
+template<typename TM> using matrix=vector<vector<TM>>;
+using graph=matrix<int>;
+template<typename TM> using tensor=vector<matrix<TM>>;
+template<typename TM> using hypermatrix=vector<tensor<TM>>;
+template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, Args... args){
+	if constexpr(sizeof...(args) == 0){
+		return vector<TM>(first, Val);
+	} else {
+		return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
+	}
+}
+#define all(x) (x).begin(),(x).end()
+#define forn(i,n) for(int i=0;i<(n);++i)
+#define MULTITEST true
+#define pb push_back
+void solve(){
+	int n,m;in(n,m);
+	vector<int> mx(m-n+1);
+	function<int(int)> query = [&](int i)->int{
+        int ret = 0;
+        for(; i>=0; i=(i&i+1)-1)
+            ret = max(ret, mx[i]);
+        return ret;
+    };
+    function<void(int, int)> update = [&](int i, int val){
+        for(; i<=m-n; i|=i+1)
+            mx[i] = max(mx[i], val);
+    };
+    function<int(int, int)> v = [](int b, int x)->int{
+        int ans = 0;
+        while(!(x % b)){
+            ans++;
+            x /= b;
+        }
+        return ans;
+    };
+	forn(i,n-1){
+		for(int j=(m-n+(i+2))/(i+2)*(i+2);j>i+1;j-=i+2){
+			update(j-(i+2), query(j-(i+2))+v(i+2, j));
+		}
+	}
+	out(query(m-n));
+}
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	int t=1;
+	if (MULTITEST) cin>>t;
+	forn(i,t)solve();
+}
+
+
