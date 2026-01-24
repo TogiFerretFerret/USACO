efb8dde3d3cfe6b78f2f3c8c62abf1da1d1bd207
Riverxia <riverdalecoder@outlook.com>
2026-01-24 13:49:10 -0500

    movcc will eternally be my new compiler xddd

--- G. Xor-MST.cpp
+++ G. Xor-MST.cpp
@@ -31,11 +31,11 @@ using graph=matrix<int>;
 template<typename TM> using tensor=vector<matrix<TM>>;
 template<typename TM> using hypermatrix=vector<tensor<TM>>;
 template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, Args... args){
-	if constexpr(sizeof...(args) == 0){
-		return vector<TM>(first, Val);
-	} else {
-		return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
-	}
+    if constexpr(sizeof...(args) == 0){
+        return vector<TM>(first, Val);
+    } else {
+        return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
+    }
 }
 #define all(x) (x).begin(),(x).end()
 #define forn(i,n) for(int i=0;i<(n);++i)
@@ -43,142 +43,92 @@ template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, A
 #define fOrn(v,s,e) for(int v=(s);v<(e);++v)
 #define INTERACTIVE false
 #if INTERACTIVE
-m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << endl; }//softmod for interactive
+m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << endl; }
 m1(debug) { cerr << forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
 m1(in) { cin >> forward<T>(a); m2(cin >>); }
 #else
-m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << "\n"; }//softmod for interactive
+m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << "\n"; }
 m1(debug) { cerr << forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
 m1(in) { cin >> forward<T>(a); m2(cin >>); }
 #endif
 #define MULTITEST false
 #define pb push_back
-// We have two approaches:
-//// 1. Prim's Algorithm with a trie + Meet in the Middle (recursive)
-//// 2. Modified Boruvka's Algorithm with a trie <- better approach
+
 void solve(){
-	int n; in(n);
-	vector<int> a(n); in(a);
-	if(n <= 1){ out(0); return; }
-	const int B = 30; // bit depth for ai <= 1e9
+    int n; in(n);
+    vector<int> a(n); in(a);
+    sort(all(a));
+    a.erase(unique(all(a)), a.end());
+    n = a.size();
 
-	// Trie storage: children, count, and leaf id lists
-	vector<array<int,2>> ch; ch.reserve(n*(B+1));
-	vector<int> cnt; cnt.reserve(n*(B+1));
-	vector<vector<int>> leaf_ids; leaf_ids.reserve(n*(B+1));
-	function<int()> new_node = [&]()->int {
-		ch.push_back({-1,-1});
-		cnt.push_back(0);
-		leaf_ids.emplace_back();
-		return (int)ch.size() - 1;
-	};
-	int root = new_node();
+    // Max nodes approx N * 30. Pre-allocate for speed and safety.
+    int max_nodes = n * 30 + 100;
+    vector<int> ch0(max_nodes, -1), ch1(max_nodes, -1), sz(max_nodes, 0);
+    int nodes_cnt = 1; 
 
-	vector<int> posInLeaf(n, -1), whereLeaf(n, -1);
-	function<void(int,int)> trie_insert = [&](int val, int id)->void {
-		int cur = root; cnt[cur]++;
-		f0rn(b, B-1, -1){
-			int bit = (val >> b) & 1;
-			if(ch[cur][bit] == -1) ch[cur][bit] = new_node();
-			cur = ch[cur][bit]; cnt[cur]++;
-		}
-		whereLeaf[id] = cur;
-		posInLeaf[id] = (int)leaf_ids[cur].size();
-		leaf_ids[cur].push_back(id);
-	};
-	function<void(int,int)> trie_erase = [&](int val, int id)->void {
-		int cur = root; cnt[cur]--;
-		f0rn(b, B-1, -1){
-			int bit = (val >> b) & 1;
-			int nxt = ch[cur][bit];
-			cur = nxt; cnt[cur]--;
-		}
-		int leaf = cur;
-		int pos = posInLeaf[id];
-		int last = leaf_ids[leaf].back();
-		leaf_ids[leaf][pos] = last;
-		posInLeaf[last] = pos;
-		leaf_ids[leaf].pop_back();
-		posInLeaf[id] = -1;
-		whereLeaf[id] = -1;
-	};
-	function<int(int)> trie_query = [&](int val)->int {
-		if(cnt[root] == 0) return -1;
-		int cur = root;
-		f0rn(b, B-1, -1){
-			int bit = (val >> b) & 1;
-			int prefer = ch[cur][bit];
-			if(prefer != -1 && cnt[prefer] > 0) cur = prefer;
-			else cur = ch[cur][1-bit];
-		}
-		if(leaf_ids[cur].empty()) return -1;
-		return leaf_ids[cur].back();
-	};
-	function<bool()> trie_empty = [&]()->bool { return cnt[root] == 0; };
+    function<void(int)> insert = [&](int val) ->void{
+        int u = 0;
+        sz[u]++;
+        f0rn(b, 29, -1){
+            int bit = (val >> b) & 1;
+            int &nxt = (bit ? ch1[u] : ch0[u]);
+            if(nxt == -1) nxt = nodes_cnt++;
+            u = nxt;
+            sz[u]++;
+        }
+    };
+    for(int x : a) insert(x);
 
-	// DSU with members list per root
-	vector<int> parent(n);
-	vector<vector<int>> members(n);
-	forn(i,n){ parent[i] = i; members[i].push_back(i); }
-	function<int(int)> find = [&](int x)->int { return parent[x] == x ? x : parent[x] = find(parent[x]); };
-	function<bool(int,int)> unite = [&](int x, int y)->bool {
-		x = find(x); y = find(y); if(x == y) return false;
-		// union by size (members size)
-		if(members[x].size() < members[y].size()) swap(x,y);
-		parent[y] = x;
-		forn(ii, (int)members[y].size()) members[x].push_back(members[y][ii]);
-		members[y].clear();
-		return true;
-	};
+    function<int(int,int,int)> query = [&](int u, int val, int b)->int {
+        int res = 0;
+        for(int i = b; i >= 0; --i){
+            int bit = (val >> i) & 1;
+            int desired = bit;
+            int nxt = (desired ? ch1[u] : ch0[u]);
+            if(nxt != -1){
+                u = nxt;
+            } else {
+                res |= (1 << i);
+                u = (desired ? ch0[u] : ch1[u]);
+            }
+        }
+        return res;
+    };
 
-	// build initial trie
-	forn(i,n) trie_insert(a[i], i);
+    function<ll(int,int,int,int)> dfs = [&](int u, int l, int r, int b) -> ll {
+        if(b < 0 || r - l <= 1) return 0;
+        int c0 = ch0[u], c1 = ch1[u];
+        // Since a is sorted, sz[c0] (if exists) is exactly the count of elements with bit=0
+        int mid = l + (c0 == -1 ? 0 : sz[c0]);
+        
+        ll ans = 0;
+        if(c0 != -1) ans += dfs(c0, l, mid, b - 1);
+        if(c1 != -1) ans += dfs(c1, mid, r, b - 1);
+        
+        if(c0 != -1 && c1 != -1){
+            ll min_w = -1;
+            // Iterate over the smaller set to find the best edge connecting the two components
+            if(mid - l < r - mid){
+                for(int i = l; i < mid; ++i){
+                    ll cost = (1LL << b) + query(c1, a[i], b - 1);
+                    if(min_w == -1 || cost < min_w) min_w = cost;
+                }
+            } else {
+                for(int i = mid; i < r; ++i){
+                    ll cost = (1LL << b) + query(c0, a[i], b - 1);
+                    if(min_w == -1 || cost < min_w) min_w = cost;
+                }
+            }
+            ans += min_w;
+        }
+        return ans;
+    };
 
-	ll ans = 0;
-	int components = n;
-	while(components > 1){
-		vector<int> roots;
-		forn(i,n) if(find(i) == i) roots.push_back(i);
-		vector<ll> bestCost(n, (1LL<<60));
-		vector<int> bestU(n, -1), bestV(n, -1);
-
-		forn(ri, (int)roots.size()){
-			int r = roots[ri];
-			// remove all members of component r
-			forn(ii, (int)members[r].size()){
-				int v = members[r][ii]; trie_erase(a[v], v);
-			}
-			// query best outgoing edge
-			forn(ii, (int)members[r].size()){
-				int u = members[r][ii];
-				if(trie_empty()) break;
-				int id = trie_query(a[u]);
-				if(id == -1) continue;
-				ll cost = (ll)(a[u] ^ a[id]);
-				if(cost < bestCost[r]){ bestCost[r] = cost; bestU[r] = u; bestV[r] = id; }
-			}
-			// reinsert members
-			forn(ii, (int)members[r].size()){
-				int v = members[r][ii]; trie_insert(a[v], v);
-			}
-		}
-
-		bool any_merge = false;
-		forn(ri, (int)roots.size()){
-			int r = roots[ri];
-			if(bestU[r] != -1){
-				if(unite(bestU[r], bestV[r])){ ans += bestCost[r]; components--; any_merge = true; }
-			}
-		}
-		if(!any_merge) break; // safety: shouldn't happen
-	}
-	out(ans);
+    out(dfs(0, 0, n, 29));
 }
 int main(){
-	if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
-	int t=1;
-	if (MULTITEST) cin>>t;
-	forn(i,t)solve();
+    if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
+    int t=1;
+    if (MULTITEST) cin>>t;
+    forn(i,t)solve();
 }
-
-
