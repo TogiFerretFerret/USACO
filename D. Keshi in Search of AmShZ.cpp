cc2242a7867ca071bddd4c909cd02ed20a1764ec
Riverxia <riverdalecoder@outlook.com>
2026-01-25 11:20:35 -0500

    test

--- D. Keshi in Search of AmShZ.cpp
+++ D. Keshi in Search of AmShZ.cpp
@@ -1,76 +1,72 @@
-// Problem: D. Keshi in Search of AmShZ
-// Judge: Codeforces
-// URL: https://codeforces.com/gym/666903/problem/D
-// Memory Limit: 256 MB
-// Time Limit: 2000 ms
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
 using namespace std;
-typedef long long ll;
-static const uint64_t CHRONO_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-static const uint64_t PIDRNDP = (uint64_t)(getpid())*0xbffbffbffULL;
-static mt19937_64 PIDRNG(PIDRNDP);
-static const uint64_t PIDRND = PIDRNG();
-static const uint64_t FIXED_RANDOM = CHRONO_RANDOM ^ PIDRND;
-struct CHASH {
-    template <typename T> size_t operator()(const T& x) const {
-        return hash<T>{}(x) ^ FIXED_RANDOM;
-    }
-    template <typename T1, typename T2> size_t operator()(const pair<T1, T2>& x) const {
-        return (*this)(x.first) ^ ((*this)(x.second) + 0x9e3779b9 + (x.first << 6) + (x.first >> 2));
-    }
-};
-template<class T, class U> istream& operator>>(istream& i, pair<T, U>& p) { return i >> p.first >> p.second; }
-template<class T, class U> ostream& operator<<(ostream& o, const pair<T, U>& p) { return o << p.first << " " << p.second; }
-template<class T> istream& operator>>(istream& i, vector<T>& v) { for(auto& x : v) i >> x; return i; }
-template<class T> ostream& operator<<(ostream& o, const vector<T>& v) { for(int i=0; i<v.size(); ++i) o << v[i] << (i==v.size()-1?"":" "); return o; }
-#define m1(x) template<class T, class... U> void x(T&& a, U&&... b)
-#define m2(x) (int[]){(x forward<U>(b),0)...}
-template<typename T1,typename T2> using hashmap=unordered_map<T1,T2,CHASH>;
-template<typename TM> using matrix=vector<vector<TM>>;
-using graph=matrix<int>;
-template<typename TM> using tensor=vector<matrix<TM>>;
-template<typename TM> using hypermatrix=vector<tensor<TM>>;
-template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, Args... args){
-	if constexpr(sizeof...(args) == 0){
-		return vector<TM>(first, Val);
-	} else {
-		return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
-	}
-}
-#define all(x) (x).begin(),(x).end()
-#define forn(i,n) for(int i=0;i<(n);++i)
-#define f0rn(v,s,e) for(int v=(s);v>(e);--v)
-#define fOrn(v,s,e) for(int v=(s);v<(e);++v)
-#define INTERACTIVE false
-#ifndef LOCAL_JUDGE
-#define FILEMODE false
-#define FILENAME "pname"
-#endif
-#if INTERACTIVE
-m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << endl; }//softmod for interactive
-m1(debug) { cerr << forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
-m1(in) { cin >> forward<T>(a); m2(cin >>); }
-#else
-m1(out) { cout << forward<T>(a);  m2(cout << " " <<); cout << "\n"; }//softmod for interactive
-m1(debug) { cerr << forward<T>(a);  m2(cerr << " " <<); cerr << "\n"; }
-m1(in) { cin >> forward<T>(a); m2(cin >>); }
-#endif
-#define MULTITEST false
-#define pb push_back
-void solve(){
-	
-}
-int main(){
-	if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
-	#ifndef LOCAL_JUDGE
-	#if FILEMODE
-	freopen(FILENAME".in","r",stdin);
-	freopen(FILENAME".out","w",stdout);
-	#endif
-	#endif
-	int t=1;
-	if (MULTITEST) cin>>t;
-	forn(i,t)solve();
-}
 
+const int INF = 1e9;
 
+int main() {
+    // Faster I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    if (!(cin >> n >> m)) return 0;
+
+    // We process the graph backwards from N to 1.
+    // Graph 'adj' stores reversed edges: v -> u if original has u -> v
+    vector<vector<int>> adj(n + 1);
+    vector<int> out_degree(n + 1, 0);
+
+    for (int i = 0; i < m; ++i) {
+        int u, v;
+        cin >> u >> v;
+        // Edge u -> v exists
+        out_degree[u]++;
+        adj[v].push_back(u); // Reverse for propagation
+    }
+
+    // dist[u] = minimal days to guarantee arrival at N
+    vector<int> dist(n + 1, INF);
+    // processed_count[u] = how many neighbors of u have been settled so far
+    vector<int> processed_count(n + 1, 0);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+    dist[n] = 0;
+    pq.push({0, n});
+
+    while (!pq.empty()) {
+        int d = pq.top().first;
+        int u = pq.top().second;
+        pq.pop();
+
+        if (d > dist[u]) continue;
+        if (u == 1) {
+            cout << d << endl;
+            return 0;
+        }
+
+        for (int v : adj[u]) {
+            // Propagate distance to v
+            // v has edge to u. u is a neighbor of v.
+            // AmShZ keeps u as an option.
+            processed_count[v]++;
+            
+            // Current best neighbor is u with dist[u].
+            // To ensure worst case is u, we block all OTHER remaining (worse) neighbors.
+            // Cost = dist[u] (worst allowed move) + 1 (day to move) + remaining_bad (days to block)
+            int remaining_neighbors = out_degree[v] - processed_count[v];
+            int candidate = dist[u] + 1 + remaining_neighbors;
+
+            if (candidate < dist[v]) {
+                dist[v] = candidate;
+                pq.push({candidate, v});
+            }
+        }
+    }
+
+    return 0;
+}
