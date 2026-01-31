a8da5f81a73855f5584b936928ab59583c50379f
Riverxia <riverdalecoder@outlook.com>
2026-01-30 20:19:55 -0500

    (acm-kakasi) Currently Listening To: [MEME 2] takehirotei - Harumachi Power  - takehirotei

--- solved/cf-pke3/D. Purple Crayon.cpp
+++ solved/cf-pke3/D. Purple Crayon.cpp
@@ -31,11 +31,11 @@ using graph=matrix<int>;
 template<typename TM> using tensor=vector<matrix<TM>>;
 template<typename TM> using hypermatrix=vector<tensor<TM>>;
 template<typename TM, TM Val = TM(), typename... Args> auto make(size_t first, Args... args){
-	if constexpr(sizeof...(args) == 0){
-		return vector<TM>(first, Val);
-	} else {
-		return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
-	}
+    if constexpr(sizeof...(args) == 0){
+        return vector<TM>(first, Val);
+    } else {
+        return vector<decltype(make<TM, Val>(args...))>(first, make<TM, Val>(args...));
+    }
 }
 #define all(x) (x).begin(),(x).end()
 #define forn(i,n) for(int i=0;i<(n);++i)
@@ -58,19 +58,94 @@ m1(in) { cin >> std::forward<T>(a); m2(cin >>); }
 #define MULTITEST false
 #define pb push_back
 void solve(){
-	
+    int n, k;
+    in(n, k);
+
+    graph adj(n + 1);
+    forn(i, n - 1) {
+        int u, v;
+        in(u, v);
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    vector<int> chain_lens;
+
+    function<int(int, int)> dfs = [&](int u, int p) -> int {
+        int max_h = 0;
+        for (int v : adj[u]) {
+            if (v == p) continue;
+            int h = dfs(v, u);
+            if (h > max_h) {
+                if (max_h > 0) chain_lens.pb(max_h);
+                max_h = h;
+            } else {
+                chain_lens.pb(h);
+            }
+        }
+        return max_h + 1;
+    };
+
+    int root_chain = dfs(1, 0);
+    chain_lens.pb(root_chain);
+
+    sort(chain_lens.rbegin(), chain_lens.rend());
+
+    ll max_score = -4e18; 
+    ll current_S = 0;
+
+    int limit = min((int)chain_lens.size(), k);
+    
+    for (int x = 1; x <= limit; ++x) {
+        current_S += chain_lens[x - 1];
+
+        vector<ll> r_candidates;
+        r_candidates.pb(x);
+        r_candidates.pb(k);
+        ll mid1 = n / 2;
+        ll mid2 = (n + 1) / 2;
+        if (mid1 >= x && mid1 <= k) r_candidates.pb(mid1);
+        if (mid2 >= x && mid2 <= k) r_candidates.pb(mid2);
+
+        ll current_config_max = -4e18;
+
+        for (ll r_val : r_candidates) {
+            ll b_limit = n - current_S;
+            vector<ll> b_candidates;
+            b_candidates.pb(b_limit);
+            if (mid1 <= b_limit) b_candidates.pb(mid1);
+            if (mid2 <= b_limit) b_candidates.pb(mid2);
+            
+            ll best_blue_response_score = 4e18;
+            
+            for (ll b_val : b_candidates) {
+                ll score = (ll)(n - r_val - b_val) * (r_val - b_val);
+                if (score < best_blue_response_score) {
+                    best_blue_response_score = score;
+                }
+            }
+            
+            if (best_blue_response_score > current_config_max) {
+                current_config_max = best_blue_response_score;
+            }
+        }
+        
+        if (current_config_max > max_score) {
+            max_score = current_config_max;
+        }
+    }
+
+    out(max_score);
 }
 int main(){
-	if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
-	#ifndef LOCAL_JUDGE
-	#if FILEMODE
-	freopen(FILENAME".in","r",stdin);
-	freopen(FILENAME".out","w",stdout);
-	#endif
-	#endif
-	int t=1;
-	if (MULTITEST) cin>>t;
-	forn(i,t)solve();
+    if(!INTERACTIVE)cin.tie(0)->sync_with_stdio(0);
+    #ifndef LOCAL_JUDGE
+    #if FILEMODE
+    freopen(FILENAME".in","r",stdin);
+    freopen(FILENAME".out","w",stdout);
+    #endif
+    #endif
+    int t=1;
+    if (MULTITEST) cin>>t;
+    forn(i,t)solve();
 }
-
-
